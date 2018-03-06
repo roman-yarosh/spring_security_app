a9d526364b2f5867c0b01fad90cbfcc8fd2f92fe
Roman Yarosh <yracompany@gmail.com>
2018-03-06 21:32:51 +0200

    50 min

--- src/java/com/yra/springsecurity/validator/UserValidator.java
+++ src/java/com/yra/springsecurity/validator/UserValidator.java
@@ -1,8 +1,47 @@
 package com.yra.springsecurity.validator;
 
+import com.yra.springsecurity.model.User;
+import com.yra.springsecurity.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
 /**
  * Created by YaroshR on 06.03.2018.
  */
-public class UserValidator {
-    video 38:15
+@Component
+public class UserValidator implements Validator{
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        User user = (User) o;
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "Required");
+
+        if (user.getUsername().length() < 8 || user.getUsername().length() > 32) {
+            errors.rejectValue("username", "Size.userForm.username");
+        }
+
+        if (userService.findByUsername(user.getUsername()) != null) {
+            errors.rejectValue("username", "Duplicate.userForm.username");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "Required");
+        if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
+            errors.rejectValue("password", "Size.userForm.password");
+        }
+
+        if (!user.getPassword().equals(user.getConfirmPassword())){
+            errors.rejectValue("confirmPassword", "Different.userForm.password");
+        }
+    }
 }
