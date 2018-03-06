f09950f4f62d041f137cb2b87f4ff4d9801df033
roman.yarosh <yracompany@gmail.com>
2018-03-06 17:37:09 +0200

    RY

--- src/java/com/yra/springsecurity/service/UserService.java
+++ src/java/com/yra/springsecurity/service/UserService.java
@@ -1,7 +1,13 @@
 package com.yra.springsecurity.service;
 
+import com.yra.springsecurity.model.User;
+
 /**
  * Created by YaroshR on 06.03.2018.
  */
 public interface UserService {
+
+    void save(User user);
+
+    User findByUsername(String username);
 }
